d1f18cdff0dc2103db846ace31bdf518b653f31f
Kenichiro Sato <satok0702@gmail.com>
2016-03-27 10:56:41 +0900

    changed directory configuration and import

--- CatDogTube/UIScrollView+extension.swift
+++ CatDogTube/UIScrollView+extension.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2016年 Kenichiro Sato. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
 extension UIScrollView {
--- CatDogTube/Video.swift
+++ CatDogTube/Video.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2016年 Kenichiro Sato. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
 class Video: NSObject {
     
