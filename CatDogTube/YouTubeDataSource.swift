1430d50e97e287adfc4b34ea61c45934b5dfdf04
Kenichiro Sato <satok0702@gmail.com>
2016-06-04 19:14:28 +0900

    added before/after time

--- CatDogTube/YouTubeDataSource.swift
+++ CatDogTube/YouTubeDataSource.swift
@@ -33,6 +33,15 @@ class YouTubeDataSource: NSObject, SearchVideoDataSourceProtocol {
             let after = publishedDate.after{
             params["publishedBefore"] = before
             params["publishedAfter"] = after
+            //cat
+            //           params["publishedBefore"] = "2015-03-13T15:00:26Z"//2014-02-18T17:30:05Z
+            //           params["publishedAfter"] =  "2015-02-11T15:00:26Z"//2014-01-19T17:30:05Z
+            
+            //dog
+            //           params["publishedBefore"] = /*"2013-08-12T18:01:59Z"*/"2012-09-04T05:47:54Z"
+            //             params["publishedAfter"] =  /*"2013-07-13T18:01:59Z"*/"2012-08-05T05:47:54Z"
+            
+
             print("before:" + before + " after:" + after)
         }
         
