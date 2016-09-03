7f37f6b5b2157d0bccf82fed90329fa1f00dc52d
Kenichiro Sato <satok0702@gmail.com>
2016-09-03 09:08:57 +0900

    exclude inappropriate videos

---
+++ CatDogTube/VideoExcluder.swift
@@ -0,0 +1,78 @@
+//
+//  VideoExcluder.swift
+//  CatDogTube
+//
+//  Created by Kenichiro Sato on 2016/09/02.
+//  Copyright © 2016年 Kenichiro Sato. All rights reserved.
+//
+
+import UIKit
+
+class VideoExcluder: NSObject {
+
+    static private let NG_WORDS:[String] = [
+        "シュレディンガー",
+        "ワルツ",
+        "おまわりさん",
+        "オッパイ",
+        "織田裕二",
+        "マーチ",
+        "Greeeen",
+        "力士",
+        "Lineage",
+        "プルー",
+        "食糞",
+        "ヨンヨン",
+        "ソフトバンク",
+        "アンドロメダ",
+        "えっち",
+        "メイク",
+        "Mmd",
+        "なめこ",
+        "迷子",
+        "Mimosa",
+        "難波",
+        "ピアノ",
+        "ツチグモ",
+        "3D",
+        "MGS",
+        "クモ",
+        "アニメ",
+        "CM",
+        "ひまわり",
+        "はねられ",
+        "マツコ",
+        "nikon",
+        "東方",
+        "性",
+        "虐待",
+        "合唱",
+        "ダンス",
+        "物語",
+        "死",
+        "エロ",
+        "嵐",
+        "マイクラ",
+        "運動会",
+        "閲覧注意",
+        "リンパ",
+        "初音",
+        "Episode",
+        "プリキュア",
+        "ミケランジェロ",
+        "メガネ"
+        ]
+    
+    class func excludeInappropriateVideos(videos:[Video]?) -> [Video]? {
+        return videos?.filter{!VideoExcluder.isInappropriateVideo($0)}
+    }
+    
+    private class func isInappropriateVideo(video:Video) -> Bool {
+        for ngWord in VideoExcluder.NG_WORDS {
+            if (video.title.lowercaseString.containsString(ngWord.lowercaseString)) {
+                return true
+            }
+        }
+        return false
+    }
+}
