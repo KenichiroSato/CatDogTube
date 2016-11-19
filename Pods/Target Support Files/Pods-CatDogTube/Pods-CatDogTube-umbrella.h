9a23396b2c87d19d76db8d614dca6313d1cc87f3
Kenichiro Sato <satok0702@gmail.com>
2016-11-19 10:14:42 +0900

    updated to cocoapods 1.1.1

---
+++ Pods/Target Support Files/Pods-CatDogTube/Pods-CatDogTube-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_CatDogTubeVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CatDogTubeVersionString[];
+
---
+++ Pods/Target Support Files/Pods-CatDogTubeTests/Pods-CatDogTubeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CatDogTubeTests : NSObject
+@end
+@implementation PodsDummy_Pods_CatDogTubeTests
+@end
