9ef04570742e3f5df69a6d1ea2643208586c90a1
Kenichiro Sato <satok0702@gmail.com>
2016-12-16 23:03:01 +0900

    added Extension framework

---
+++ Extension/Extension.h
@@ -0,0 +1,19 @@
+//
+//  Extension.h
+//  Extension
+//
+//  Created by ken on 2016/12/16.
+//  Copyright © 2016年 Kenichiro Sato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Extension.
+FOUNDATION_EXPORT double ExtensionVersionNumber;
+
+//! Project version string for Extension.
+FOUNDATION_EXPORT const unsigned char ExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Extension/PublicHeader.h>
+
+
